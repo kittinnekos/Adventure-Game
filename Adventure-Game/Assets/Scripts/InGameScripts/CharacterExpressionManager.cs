16bdd03ddbb3272ca0fdc747d9e057a416d0de27
kittinnekos <131129020+kittinnekos@users.noreply.github.com>
2024-07-23 10:44:44 +0900

    [add]キャラクターの立ち絵と表情を別々に表示させるスクリプトを追加

--- Adventure-Game/Assets/Scripts/InGameScripts/CharacterExpressionManager.cs
+++ Adventure-Game/Assets/Scripts/InGameScripts/CharacterExpressionManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,11 +7,14 @@ namespace AdventureGame
 {
     public class CharacterExpressionManager : MonoBehaviour
     {
+        // カラーアルファ値の最大値と最低値
         private float maxAlphaNum = 1f;
         private float minAlphaNum = 0f;
 
-        // アクティブな子オブジェクト（表情）を取得する
-        private GameObject FindActiveChildObject(RectTransform parentRT)
+        public float fadeIn_OutExpressionTimer = 0.1f;
+
+        // アクティブな子オブジェクト（表情）を取得する（最初にアクティブな物のみ）
+        public GameObject FindActiveChildObject(RectTransform parentRT)
         {
             foreach(RectTransform childRT in parentRT)
             {
@@ -23,6 +27,25 @@ namespace AdventureGame
             return null;
         }
 
+        // 全てのアクティブな子オブジェクト（表情）を取得する
+        public List<GameObject> FindActiveChildObjects(RectTransform parentRT)
+        {
+            List<GameObject> activeChildObjects = new List<GameObject>();
+            foreach(RectTransform childRT in parentRT)
+            {
+                if(childRT.gameObject.activeSelf)
+                {
+                    activeChildObjects.Add(childRT.gameObject);
+                }
+            }
+
+            if(activeChildObjects.Count == 0)
+            {
+                Debug.Log("Not Active ChildObject");
+            }
+            return activeChildObjects;
+        }
+
         // 子オブジェクト(表情)のフェードイン
         public IEnumerator FadeInExpression(GameObject childObject)
         {
@@ -31,9 +54,9 @@ namespace AdventureGame
             color.a = 0;
             childObject.SetActive(true);
 
-            while(!Mathf.Approximately(color.a, maxAlphaNum)) // 透明度がほぼ等しくなるまで繰り返す
+            while(!Mathf.Approximately(color.a, maxAlphaNum))// 透明度がほぼ等しくなるまで繰り返す
             {
-                float changePerFrame = Time.deltaTime/0.5f;
+                float changePerFrame = Time.deltaTime/fadeIn_OutExpressionTimer;
                 color.a = Mathf.MoveTowards(color.a, maxAlphaNum, changePerFrame);
                 image.material.color = color;
                 yield return null;
@@ -46,9 +69,9 @@ namespace AdventureGame
             GameObject activeChildObj = FindActiveChildObject(parentRT);
             Image actImage = activeChildObj.GetComponent<Image>();
             Color actColor = actImage.material.color;
-            while(!Mathf.Approximately(actColor.a, minAlphaNum)) // 透明度がほぼ等しくなるまで繰り返す
+            while(!Mathf.Approximately(actColor.a, minAlphaNum))// 透明度がほぼ等しくなるまで繰り返す
             {
-                float changePerFrame = Time.deltaTime/0.5f;
+                float changePerFrame = Time.deltaTime/fadeIn_OutExpressionTimer;
                 actColor.a = Mathf.MoveTowards(actColor.a, minAlphaNum, changePerFrame);
                 actImage.material.color = actColor;
                 yield return null;
@@ -57,8 +80,10 @@ namespace AdventureGame
         }
 
         // 子オブジェクト(表情)の切り替え
-        public IEnumerator ExpressionBrightnessSwitcher(RectTransform parentRT , GameObject childObject)
+        public IEnumerator ChangeExpression(RectTransform parentRT , GameObject childObject)
         {
+            // 既にアクティブだった時コルーチンから抜ける
+            if(childObject.activeSelf == true) yield break;
             // アクティブな子オブジェクトをフェードアウト
             StartCoroutine(FadeOutExpression(parentRT));
             yield return null;
--- Adventure-Game/Assets/Scripts/InGameScripts/CharacterManager2.cs
+++ Adventure-Game/Assets/Scripts/InGameScripts/CharacterManager2.cs
@@ -16,17 +16,20 @@ namespace AdventureGame
         }
 
         // 表情の名前とナンバーを格納するクラス
+        [System.Serializable]
         public class ExpressionData
         {
             public string expressionName;
             public int expressionPosNum;// ナンバーはキャラクターオブジェクトからの位置を元に当てはめる。
         }
 
-        [System.NonSerialized] public Dictionary<string, GameObject> NameToPrefab;
+        [System.NonSerialized] public Dictionary<string, GameObject> NameToPrefab = new Dictionary<string, GameObject>();
 
-        [System.NonSerialized] public Dictionary<string, GameObject> NameToCharacterObject;
+        [System.NonSerialized] public Dictionary<string, GameObject> NameToCharacterObject = new Dictionary<string, GameObject>();
 
-        [System.NonSerialized] public Dictionary<string, int> expressionToNum;
+        [System.NonSerialized] public Dictionary<string, int> expressionToNum = new Dictionary<string, int>();
+
+        [SerializeField] GameObject CharacterObjects;
 
         // インスペクター上で設定
         public CharaData[] charaDatas;
@@ -35,45 +38,61 @@ namespace AdventureGame
         // インスタンス化したキャラクターオブジェクトを保管する配列(要素数は表示上限)
         private GameObject[] characterObject = new GameObject[4];
 
+        // カラーアルファ値の最大値と最低値
         private float maxAlphaNum = 1f;
         private float minAlphaNum = 0f;
 
+        private float fadeIn_OutExpressionTimer;
+        private float fadeIn_OutStandingPictureTimer = 0.2f;
+
         void Awake()
         {
+            fadeIn_OutExpressionTimer = GameManager.Instance.characterExpressionManager.fadeIn_OutExpressionTimer;
+            //int i = 0;
             // ディキショナリーの初期化
             foreach(CharaData charaData in charaDatas)
             {
+                // TODO 後のデバッグのため一時保存
+                // Debug.Log(charaData.charaName);
+                // Debug.Log(charaData.charaPrefab);
+                // Debug.Log(i);
+                // i++;
                 NameToPrefab.Add(charaData.charaName, charaData.charaPrefab);
             }
+            //int j = 0;
             foreach(ExpressionData expressionData in expressionDatas)
             {
+                // TODO 後のデバッグのため一時保存
+                // Debug.Log(expressionData.expressionName);
+                // Debug.Log(j);
+                // j++;
                 expressionToNum.Add(expressionData.expressionName, expressionData.expressionPosNum);
             }
         }
 
-        // void Update()
-        // {
-            // CharacterBrightnessSwitcherを入れる。
-        // }
+        void Update()
+        {
+            GameManager.Instance.characterBrightnessSwitcher2.CharaBrightnessSwitcher(characterObject);
+        }
 
         // 立ち絵をインスタンス化し、表情を付ける。
         public void SpawnStandingPicture(string charaName, string expression)
         {
             for(int i = 0;i < characterObject.Length;i++)
             {
-                Debug.Log(i);
+                Debug.Log("Spawn Object Num." + i);
                 if(characterObject[i] != null) continue;
-                characterObject[i] = Instantiate(NameToPrefab[charaName]);
-                
-                StartCoroutine(FadeInStandingPicture(characterObject[i]));// 立ち絵のフェードインコルーチン
-                NameToCharacterObject.Add(charaName, characterObject[i]); // ディキショナリーにインスタンス化したオブジェクトを追加
+                characterObject[i] = Instantiate(NameToPrefab[charaName], CharacterObjects.transform);
+
+                // ディキショナリーにインスタンス化したオブジェクトを追加
+                NameToCharacterObject.Add(charaName, characterObject[i]);
                 break;
             }
             RectTransform objectRT = NameToCharacterObject[charaName].GetComponent<RectTransform>();
             GameObject childObject = FindChildObject(objectRT, expression);
             if(childObject == null) return;
-            // TODO マージ後追加
-            //StartCoroutine(GameManager.Instance.characterExpressionManager.ExpressionFadeIn(childObject));
+            // 立ち絵と表情をフェードインさせるコルーチン
+            StartCoroutine(FadeInStandingPictureANDExpression(NameToCharacterObject[charaName], childObject));
         }
 
         // 表情を変える
@@ -82,8 +101,8 @@ namespace AdventureGame
             RectTransform objectRT = NameToCharacterObject[charaName].GetComponent<RectTransform>();
             GameObject childObject = FindChildObject(objectRT, expression);
             if(childObject == null) return;
-            // TODO マージ後追加
-            //StartCoroutine(GameManager.Instance.characterExpressionManager.ExpressionBrightnessSwitcher(objectRT, childObject));
+            
+            StartCoroutine(GameManager.Instance.characterExpressionManager.ChangeExpression(objectRT, childObject));
         }
 
         // 立ち絵をフェードアウト&削除
@@ -119,7 +138,7 @@ namespace AdventureGame
 
             while(!Mathf.Approximately(color.a, maxAlphaNum)) // 透明度がほぼ等しくなるまで繰り返す
             {
-                float changePerFrame = Time.deltaTime/1.0f;
+                float changePerFrame = Time.deltaTime/fadeIn_OutStandingPictureTimer;
                 color.a = Mathf.MoveTowards(color.a, maxAlphaNum, changePerFrame);
                 image.material.color = color;
                 yield return null;
@@ -134,11 +153,21 @@ namespace AdventureGame
 
             while(!Mathf.Approximately(color.a, minAlphaNum)) // 透明度がほぼ等しくなるまで繰り返す
             {
-                float changePerFrame = Time.deltaTime/1.0f;
+                float changePerFrame = Time.deltaTime/fadeIn_OutStandingPictureTimer;
                 color.a = Mathf.MoveTowards(color.a, minAlphaNum, changePerFrame);
                 image.material.color = color;
                 yield return null;
             }
+        }
+
+        // 立ち絵をフェードインさせて表情をフェードインさせる。
+        private IEnumerator FadeInStandingPictureANDExpression(GameObject characterObject, GameObject childObject)
+        {
+            // 立ち絵のフェードインコルーチン
+            StartCoroutine(FadeInStandingPicture(characterObject));
+            yield return new WaitForSeconds(fadeIn_OutStandingPictureTimer);// 立ち絵が表示されるまで待機
+            // 表情のフェードインコルーチン
+            StartCoroutine(GameManager.Instance.characterExpressionManager.FadeInExpression(childObject));
         }
 
         // 立ち絵をフェードアウトさせて削除する。
@@ -148,17 +177,18 @@ namespace AdventureGame
             {
                 // 削除する立ち絵を検索
                 if(characterObject[i] == null) continue;
+                Debug.Log("Object Active");
                 if(characterObject[i] != NameToCharacterObject[charaName]) continue;
-
+                Debug.Log("Success");
                 // 立ち絵のフェードアウト
                 RectTransform parentRT = characterObject[i].GetComponent<RectTransform>();
-                // TODO マージ後追加
-                // StartCoroutine(GameManager.Instance.characterExpressionManager.FadeOutExpression(parentRT)
-                yield return new WaitForSeconds(0.5f);// 表情をフェードアウトさせるため0.5秒待機。
+                StartCoroutine(GameManager.Instance.characterExpressionManager.FadeOutExpression(parentRT));
+                yield return new WaitForSeconds(fadeIn_OutExpressionTimer);// 表情をフェードアウトさせるため待機。
 
                 // 立ち絵の削除
                 StartCoroutine(FadeOutStandingPicture(characterObject[i]));
-                yield return new WaitForSeconds(1.0f);// 立ち絵をフェードアウトさせるため1秒待機。
+                yield return new WaitForSeconds(fadeIn_OutStandingPictureTimer);// 立ち絵をフェードアウトさせるため待機。
+                Destroy(characterObject[i]);
                 characterObject[i] = null;
 
                 // 配列内のオブジェクトを削除後ディキショナリー内のキャラクターオブジェクトを削除する
